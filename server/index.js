c20844cbd31731a4dcc307316e06f2ae220dc28c
thor1405 <johancolaco100@gmail.com>
2026-06-25 16:25:06 +0300

    fix: make express-mongo-sanitize compatible with Express 5 request getters

--- server/index.js
+++ server/index.js
@@ -33,7 +33,11 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(mongoSanitize());
+app.use((req, res, next) => {
+  if (req.body) req.body = mongoSanitize.sanitize(req.body);
+  if (req.params) req.params = mongoSanitize.sanitize(req.params);
+  next();
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
