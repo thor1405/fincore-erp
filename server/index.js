f3a97b21b488c0cdd00d10b5108cbf58167fb768
thor1405 <johancolaco100@gmail.com>
2026-06-25 16:17:22 +0300

    security: add helmet security headers and mongo sanitization middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 const authRoutes = require('./routes/auth');
 const transactionRoutes = require('./routes/transactions');
 const dashboardRoutes = require('./routes/dashboard');
@@ -27,6 +29,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(helmet());
+app.use(mongoSanitize());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
